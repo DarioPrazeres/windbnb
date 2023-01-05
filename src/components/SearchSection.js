46f1f92e1077c525cb3e2850b85b11cfeacdbf44
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-01-05 23:56:14 +0100

    update search seaction

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -1,17 +1,35 @@
 import React from "react";
 import searchIcon from "./imgComponent/searchWhite.png";
-import dataFile from "./stays.json";
+//import dataFile from "./stays.json";
+import gpsIcon from "./imgComponent/gpsIcon.png";
+
+const cities =[
+    "Helsinki",
+    "Vaasa",
+    "Turku",
+    "Oulu"
+]
+
 export default function SearchSeaction() {
+    const [searchTerm, setSearchTerm] = React.useState('');
+    const [searchResults, setSearchResults] = React.useState([]);
+    const handleChange = e =>{
+        setSearchTerm(e.target.value);
+    };
+    React.useEffect(()=>{
+        const results = cities.filter(city => city.toLocaleLowerCase().includes(searchTerm));
+        setSearchResults(results);
+    }, [searchTerm])
     return (
         <section className="search">
             <div className="search-form">
                 <div className="input-form">
                     <label>Location</label>
-                    <input type='text' placeholder="Add Location"/>
+                    <input type='text' placeholder="Add Location" value={searchTerm} onChange={handleChange} />
                 </div>
                 <div className="input-form">
                     <label>Guest</label>
-                    <input type='text' placeholder="Add Guest"/>
+                    <input type='number' placeholder="Add Guest"/>
                 </div>
                 <div>
                    <button className="submitSearch">
@@ -21,22 +39,42 @@ export default function SearchSeaction() {
                 </div>
             </div>
             <div className="search-detail">
-                <div className="detail-city">City Search</div>
-                <div className="detail-guest">Add Guest</div>
+                <div className="detail-city">
+                    {
+                        searchResults.map(item => (
+                            <CitySearch cityName={item} />
+                        ))
+                    }
+                </div>
+                <div className="detail-guest">
+                    <div className="addGuest">
+                        <h3>Adults</h3>
+                        <p className="rating">Ages 13 or above</p>
+                        <div className="control-Add">
+                            <button className="controls">-</button>
+                            <p className="cityname">0</p>
+                            <button className="controls control-add">+</button>
+                        </div>
+                    </div>
+                    <div className="addGuest-child">
+                        <h3>Children</h3>
+                        <p className="rating">Ages 2-13</p>
+                        <div className="control-Add">
+                            <button className="controls">-</button>
+                            <p className="cityname">0</p>
+                            <button className="controls control-add">+</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
 }
-function tipsCity(value, datta){
-    const result = [];
-    if(value === 'All'){
-        return datta;
-    }else {
-        for(var c=0; c<datta.length; c++){
-            if(datta[c].city === value){
-                result.push(datta[c])
-            }           
-        }
-        return result;
-    }
+function CitySearch(props){
+    return (
+        <div className="tipsCity">
+            <img src={gpsIcon} alt="item of location" />
+            <p>{props.cityName}, Finland</p>
+        </div>
+    )
 }
