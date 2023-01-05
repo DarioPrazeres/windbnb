669e0ed5564694d52005dc99a362af25a37e6d0b
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-01-05 14:54:34 +0100

    update city

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -1,5 +1,6 @@
 import React from "react";
-import searchIcon from "./imgComponent/searchWhite.png"
+import searchIcon from "./imgComponent/searchWhite.png";
+import dataFile from "./stays.json";
 export default function SearchSeaction() {
     return (
         <section className="search">
@@ -19,6 +20,23 @@ export default function SearchSeaction() {
                    </button>
                 </div>
             </div>
+            <div className="search-detail">
+                <div className="detail-city">City Search</div>
+                <div className="detail-guest">Add Guest</div>
+            </div>
         </section>
     )
+}
+function tipsCity(value, datta){
+    const result = [];
+    if(value === 'All'){
+        return datta;
+    }else {
+        for(var c=0; c<datta.length; c++){
+            if(datta[c].city === value){
+                result.push(datta[c])
+            }           
+        }
+        return result;
+    }
 }
